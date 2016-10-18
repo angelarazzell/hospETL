959dd9784d6bea7431b13d44a7ccfdb612a6aa50
Angela Razzell <angela.razzell@gmx.com>
2016-10-18 10:47:09 -0400

    modified:   src/generate_data/OP_check_ins.py

--- src/generate_data/OP_check_ins.py
+++ src/generate_data/OP_check_ins.py
@@ -1,9 +1,17 @@
+#Generate check in data for outpatients (read from the outpatient bookings creations
+#file), upload to storage in Amazon S3 buckets
+
 import random
 import time
 import csv
+import boto3
 from datetime import datetime, timedelta
 from dateutil.parser import parse
 
+s3 = boto3.resource('s3')
+
+#strTimeProp() and randomDate() functions are from the following stack overflow forum:
+#http://stackoverflow.com/questions/4874764/problem-with-format-datetime-in-python
 def strTimeProp(start, end, format, prop):
     """Get a time at a proportion of a range of two formatted times.
 
@@ -21,8 +29,8 @@ def strTimeProp(start, end, format, prop):
 def randomDate(start, end, prop):
     return strTimeProp(start, end, '%m/%d/%Y %I:%M %p', prop)
 
-with open('./historic_data/elective_bookings/OP_created_sat.csv','r') as fin, \
-     open('./historic_data/check_ins/OP_checkin_sat.csv','w') as fout:
+with open('./historic_data/elective_bookings/OP_created.csv','r') as fin, \
+     open('./historic_data/check_ins/OP_checkin.csv','w') as fout:
     reader = csv.reader(fin, delimiter=',')
     outputWriter = csv.writer(fout)
     count = 0
@@ -32,10 +40,8 @@ with open('./historic_data/elective_bookings/OP_created_sat.csv','r') as fin, \
             encounterno = 'OPE' + '%s' % (count)
             pathway_id = line[1]
             medicalid = line[2]
-            #adm_method = random.randint(11,13)
             adm_method = ''
-            attend_code = random.choice([0,0,0,0,0,0,0,0,0,0,0,0,2])
-            #randno = random.randrange(1,20,2)
+            attend_code = random.choice([0,0,0,0,0,0,0,0,0,0,0,0,2]))
             intended_management = line[5]
             hospital_id = line[6]
             doctor_id = line[7]
@@ -45,7 +51,9 @@ with open('./historic_data/elective_bookings/OP_created_sat.csv','r') as fin, \
             arrival_datetime = ''
             expected_discharge =  line[10]
             specialty = line[11]
-            booking_list = [encounterno, pathway_id, medicalid, adm_method, attend_code, intended_management, hospital_id, ward, doctor_id, appt_date, arrival_datetime, expected_discharge, specialty]
+            booking_list = [encounterno, pathway_id, medicalid, adm_method, \
+                attend_code, intended_management, hospital_id, ward, doctor_id, \
+                appt_date, arrival_datetime, expected_discharge, specialty]
             outputWriter.writerow(booking_list)
         elif round(random.randint(0,51)/100) == 0:
             encounterno = 'OPE' + '%s' % (count)
@@ -53,7 +61,6 @@ with open('./historic_data/elective_bookings/OP_created_sat.csv','r') as fin, \
             medicalid = line[2]
             adm_method = ''
             attend_code = random.choice([5,6])
-            #randno = random.randrange(1,20,2)
             intended_management = line[5]
             hospital_id = line[6]
             doctor_id = line[7]
@@ -63,7 +70,9 @@ with open('./historic_data/elective_bookings/OP_created_sat.csv','r') as fin, \
             arrival_datetime = (appt_date - timedelta(minutes = 65)) + timedelta(random.gauss(65, 15))
             expected_discharge =  line[10]
             specialty = line[11]
-            booking_list = [encounterno, pathway_id, medicalid, adm_method, attend_code, intended_management, hospital_id, ward, doctor_id, appt_date, arrival_datetime, expected_discharge, specialty]
+            booking_list = [encounterno, pathway_id, medicalid, adm_method, \
+                attend_code, intended_management, hospital_id, ward, doctor_id, \
+                appt_date, arrival_datetime, expected_discharge, specialty]
             outputWriter.writerow(booking_list)
         else:
             encounterno = 'OPE' + '%s' % (count)
@@ -71,7 +80,6 @@ with open('./historic_data/elective_bookings/OP_created_sat.csv','r') as fin, \
             medicalid = line[2]
             adm_method = ''
             attend_code = random.choice([2,3,4,7])
-            #randno = random.randrange(1,20,2)
             intended_management = line[5]
             hospital_id = line[6]
             doctor_id = line[7]
@@ -84,13 +92,13 @@ with open('./historic_data/elective_bookings/OP_created_sat.csv','r') as fin, \
             	arrival_datetime = ''
             expected_discharge = ''
             specialty = line[11]
-            booking_list = [encounterno, pathway_id, medicalid, adm_method, attend_code, intended_management, hospital_id, ward, doctor_id, appt_date, arrival_datetime, expected_discharge, specialty]
+            booking_list = [encounterno, pathway_id, medicalid, adm_method, \
+                attend_code, intended_management, hospital_id, ward, doctor_id, \
+                appt_date, arrival_datetime, expected_discharge, specialty]
             outputWriter.writerow(booking_list)
-#        if count == 100:
-#            break
 
-with open('./historic_data/elective_bookings/OP_modify_sat.csv','r') as fin, \
-     open('./historic_data/check_ins/OP_checkin_sat.csv','a') as fout:
+with open('./historic_data/elective_bookings/OP_modify.csv','r') as fin, \
+     open('./historic_data/check_ins/OP_checkin.csv','a') as fout:
     reader = csv.reader(fin, delimiter=',')
     outputWriter = csv.writer(fout)
     count = 0
@@ -100,10 +108,8 @@ with open('./historic_data/elective_bookings/OP_modify_sat.csv','r') as fin, \
             encounterno = 'OPEM' + '%s' % (count)
             pathway_id = line[1]
             medicalid = line[2]
-            #adm_method = random.randint(11,13)
             adm_method = ''
             attend_code = random.choice([0,0,0,0,0,0,0,0,0,0,0,0,2])
-            #randno = random.randrange(1,20,2)
             intended_management = line[5]
             hospital_id = line[6]
             doctor_id = line[7]
@@ -113,7 +119,9 @@ with open('./historic_data/elective_bookings/OP_modify_sat.csv','r') as fin, \
             arrival_datetime = ''
             expected_discharge =  line[10]
             specialty = line[11]
-            booking_list = [encounterno, pathway_id, medicalid, adm_method, attend_code, intended_management, hospital_id, ward, doctor_id, appt_date, arrival_datetime, expected_discharge, specialty]
+            booking_list = [encounterno, pathway_id, medicalid, adm_method, \
+                attend_code, intended_management, hospital_id, ward, doctor_id, \
+                appt_date, arrival_datetime, expected_discharge, specialty]
             outputWriter.writerow(booking_list)
         elif round(random.randint(0,51)/100) == 0:
             encounterno = 'OPEM' + '%s' % (count)
@@ -121,7 +129,6 @@ with open('./historic_data/elective_bookings/OP_modify_sat.csv','r') as fin, \
             medicalid = line[2]
             adm_method = ''
             attend_code = random.choice([5,6])
-            #randno = random.randrange(1,20,2)
             intended_management = line[5]
             hospital_id = line[6]
             doctor_id = line[7]
@@ -131,7 +138,9 @@ with open('./historic_data/elective_bookings/OP_modify_sat.csv','r') as fin, \
             arrival_datetime = (appt_date - timedelta(minutes = 65)) + timedelta(random.gauss(65, 15))
             expected_discharge =  line[10]
             specialty = line[11]
-            booking_list = [encounterno, pathway_id, medicalid, adm_method, attend_code, intended_management, hospital_id, ward, doctor_id, appt_date, arrival_datetime, expected_discharge, specialty]
+            booking_list = [encounterno, pathway_id, medicalid, adm_method, \
+                attend_code, intended_management, hospital_id, ward, doctor_id,\
+                appt_date, arrival_datetime, expected_discharge, specialty]
             outputWriter.writerow(booking_list)
         else:
             encounterno = 'OPEM' + '%s' % (count)
@@ -139,7 +148,6 @@ with open('./historic_data/elective_bookings/OP_modify_sat.csv','r') as fin, \
             medicalid = line[2]
             adm_method = ''
             attend_code = random.choice([2,3,4,7])
-            #randno = random.randrange(1,20,2)
             intended_management = line[5]
             hospital_id = line[6]
             doctor_id = line[7]
@@ -152,8 +160,10 @@ with open('./historic_data/elective_bookings/OP_modify_sat.csv','r') as fin, \
             	arrival_datetime = ''
             expected_discharge = ''
             specialty = line[11]
-            booking_list = [encounterno, pathway_id, medicalid, adm_method, attend_code, intended_management, hospital_id, ward, doctor_id, appt_date, arrival_datetime, expected_discharge, specialty]
+            booking_list = [encounterno, pathway_id, medicalid, adm_method, \
+                attend_code, intended_management, hospital_id, ward, doctor_id,\
+                appt_date, arrival_datetime, expected_discharge, specialty]
             outputWriter.writerow(booking_list)
-#        if count == 15:
-#            break
-        
+
+s3.meta.client.upload_file('./historic_data/check_ins/OP_checkin.csv', 'angela-hospital-data', 'historic_data/'+'OP_checkin.csv')
+      
