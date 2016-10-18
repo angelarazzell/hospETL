bfc9bec3c1374607a1b3915ebc8e9e58eb5c7416
Angela Razzell <angela.razzell@gmx.com>
2016-10-18 13:02:25 -0400

    modified report_queries

--- src/Redshift/report_queries/GUIqueries.sql
+++ src/Redshift/report_queries/GUIqueries.sql
@@ -1,72 +1,77 @@
---emergency visits and breach numbers
+--emergency visits and breach numbers for stacked bar chart
 
 CREATE OR REPLACE VIEW rep.vw_emergencycount AS 
- SELECT sum(
+  SELECT sum(
         CASE
             WHEN date_diff('hours', enc.arrival_date, enc.seen_date) > 6 THEN 1
             ELSE 0
-        END) AS breach, sum(
+        END) AS breach, 
+        sum(
         CASE
             WHEN date_diff('hours', enc.arrival_date, enc.seen_date) <= 6 THEN 1
             ELSE 0
-        END) AS nonbreach, hos.hospital_desc
-   FROM encounter enc, ref_hospital hos
+        END) AS nonbreach, 
+        hos.hospital_desc
+  FROM encounter enc, ref_hospital hos
   WHERE hos.hospital_code = enc.hospital_code AND enc.arrival_date >= date_add('day', -28, getdate())
   GROUP BY hos.hospital_desc;
 
 ------------------------------------------------------------------------------------------------------
---quarterly IP activity
-
-create or replace view rep.vw_ipsparkline as
-select hos.hospital_desc,
-  sum(case when datepart(quarter, discharge_date) = datepart(quarter,dateadd(quarter, 1, start_period))
-          and  datepart(year, discharge_date) = datepart(year, start_period)
-      then 1
-      else 0 end) as Q1,
-  sum(case when datepart(quarter, discharge_date) =  datepart(quarter,dateadd(quarter, 2, start_period))
-          and  datepart(year, discharge_date) = datepart(year, end_period)
-      then 1
-      else 0 end) as Q2,
-  sum(case when datepart(quarter, discharge_date) =  datepart(quarter,dateadd(quarter, 3, start_period))
-          and  datepart(year, discharge_date) = datepart(year, end_period)
-      then 1
-      else 0 end) as Q3,   
-  sum(case when datepart(quarter, discharge_date) =  datepart(quarter, end_period)
-          and  datepart(year, discharge_date) = datepart(year, end_period)
-      then 1
-      else 0 end) as Q4,
-  --quarter by quarter activity differences:
-  sum(case when datepart(quarter, discharge_date) = datepart(quarter, start_period)
-          and  datepart(year, discharge_date) = datepart(year, start_period)
-      then 1 else 0 end) -sum(case when datepart(quarter, discharge_date) = datepart(quarter,dateadd(quarter, 1, start_period))
-          and  datepart(year, discharge_date) = datepart(year, start_period)
-      then 1
-      else 0 end) as Q1diff,
-  sum(case when datepart(quarter, discharge_date) =  datepart(quarter,dateadd(quarter, 2, start_period))
-          and  datepart(year, discharge_date) = datepart(year, end_period)
-      then 1 else 0 end) - sum(case when datepart(quarter, discharge_date) = datepart(quarter,dateadd(quarter, 1, start_period))
-          and  datepart(year, discharge_date) = datepart(year, start_period)
-      then 1
-      else 0 end) as Q2diff,
-  sum(case when datepart(quarter, discharge_date) =  datepart(quarter,dateadd(quarter, 3, start_period))
-          and  datepart(year, discharge_date) = datepart(year, end_period)
-      then 1 else 0 end) - sum(case when datepart(quarter, discharge_date) = datepart(quarter,dateadd(quarter, 2, start_period))
-          and  datepart(year, discharge_date) = datepart(year, end_period)
-      then 1
-      else 0 end) as Q3diff,
-  sum(case when datepart(quarter, discharge_date) =  datepart(quarter,dateadd(quarter, 4, start_period))
-          and  datepart(year, discharge_date) = datepart(year, end_period)
-      then 1 else 0 end) - sum(case when datepart(quarter, discharge_date) = datepart(quarter,dateadd(quarter, 3, start_period))
-          and  datepart(year, discharge_date) = datepart(year, end_period)
-      then 1
-      else 0 end) as Q4diff
+--quarterly elective activity for sparkline and growth bars 
+create or replace view rep.vw_sparkline as
+  select hos.hospital_desc,
+    sum(case when datepart(quarter, discharge_date) = datepart(quarter,dateadd(quarter, 1, start_period))
+            and  datepart(year, discharge_date) = datepart(year, start_period)
+        then 1
+        else 0 end) as Q1,
+    sum(case when datepart(quarter, discharge_date) =  datepart(quarter,dateadd(quarter, 2, start_period))
+            and  datepart(year, discharge_date) = datepart(year, end_period)
+        then 1
+        else 0 end) as Q2,
+    sum(case when datepart(quarter, discharge_date) =  datepart(quarter,dateadd(quarter, 3, start_period))
+            and  datepart(year, discharge_date) = datepart(year, end_period)
+        then 1
+        else 0 end) as Q3,   
+    sum(case when datepart(quarter, discharge_date) =  datepart(quarter, end_period)
+            and  datepart(year, discharge_date) = datepart(year, end_period)
+        then 1
+        else 0 end) as Q4,
+    --quarter by quarter activity differences:
+    sum(case when datepart(quarter, discharge_date) = datepart(quarter, start_period)
+            and  datepart(year, discharge_date) = datepart(year, start_period)
+        then 1 else 0 end) -sum(case when datepart(quarter, discharge_date) = datepart(quarter,dateadd(quarter, 1, start_period))
+            and  datepart(year, discharge_date) = datepart(year, start_period)
+        then 1
+        else 0 end) as Q1diff,
+    sum(case when datepart(quarter, discharge_date) =  datepart(quarter,dateadd(quarter, 2, start_period))
+            and  datepart(year, discharge_date) = datepart(year, end_period)
+        then 1 else 0 end) - sum(case when datepart(quarter, discharge_date) = datepart(quarter,dateadd(quarter, 1, start_period))
+            and  datepart(year, discharge_date) = datepart(year, start_period)
+        then 1
+        else 0 end) as Q2diff,
+    sum(case when datepart(quarter, discharge_date) =  datepart(quarter,dateadd(quarter, 3, start_period))
+            and  datepart(year, discharge_date) = datepart(year, end_period)
+        then 1 else 0 end) - sum(case when datepart(quarter, discharge_date) = datepart(quarter,dateadd(quarter, 2, start_period))
+            and  datepart(year, discharge_date) = datepart(year, end_period)
+        then 1
+        else 0 end) as Q3diff,
+    sum(case when datepart(quarter, discharge_date) =  datepart(quarter,dateadd(quarter, 4, start_period))
+            and  datepart(year, discharge_date) = datepart(year, end_period)
+        then 1 else 0 end) - sum(case when datepart(quarter, discharge_date) = datepart(quarter,dateadd(quarter, 3, start_period))
+            and  datepart(year, discharge_date) = datepart(year, end_period)
+        then 1
+        else 0 end) as Q4diff,
+    booking_type
  
-from rep.vw_inpatientdc ip
-  left join ref_hospital hos
-    on hos.hospital_code = ip.hospital_id
-  cross join (select cast('30 sep 16' as datetime) as end_period,
-      cast('1 jul 15' as datetime) as start_period) dateparams
+  from rep.elec_activity ip
+    left join ref_hospital hos
+      on hos.hospital_code = ip.hospital_id
+    cross join (select cast('30 sep 16' as datetime) as end_period,
+        cast('1 jul 15' as datetime) as start_period) dateparams
   
-where discharge_date between start_period and end_period
+  where discharge_date between start_period and end_period
 
-group by hos.hospital_desc;
+  group by hos.hospital_desc, booking_type
+  order by hos.hospital_desc;
+
+
